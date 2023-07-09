5136a12cfba3d71e14a935b3e2809a44e7179de8
Tushar Anekar <anekar.tushar@gmail.com>
2023-07-09 21:56:45 +0530

    feat- city page

--- src/pages/City/City.jsx
+++ src/pages/City/City.jsx
@@ -1,13 +1,39 @@
-import { useParams } from "react-router-dom"
-import { data } from "../../db/data"
+import { Link, useParams } from "react-router-dom";
+import { data } from "../../db/data";
 
-export function City () {
-    const {continentId, countryId, cityId} = useParams()
-    const country = data.continents.find(({id}) => id === Number(continentId))
-    const city = country.countries.find(({id}) => id === Number(countryId))
-    const destination = city.find(({id}) => id === Number(cityId))
-    console.log(destination)
-    return(
-        <>City</>
-    )
+export function City() {
+  const { continentId, countryId, cityId } = useParams();
+  const country = data.continents.find(({ id }) => id === Number(continentId));
+  const { destinations } = country.countries.find(
+    ({ id }) => id === Number(countryId)
+  );
+
+  const {
+    name,
+    description,
+    image,
+    ratings,
+    reviews,
+    location,
+    openingHours,
+    ticketPrice,
+    website,
+  } = destinations.find(({ id }) => id === Number(cityId));
+  return (
+    <>
+      <h1>{name}</h1>
+      <div>
+        <img src={image} alt={name} />
+        <div>
+          <p>{description}</p>
+          <p>{ratings}</p>
+          <p>{reviews}</p>
+          <p>{location}</p>
+          <p>{openingHours}</p>
+          <p>{ticketPrice}</p>
+          <Link to={website}>{website}</Link>
+        </div>
+      </div>
+    </>
+  );
 }
